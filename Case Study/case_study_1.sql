2af58bbd30f0240fcf0682402fdcc01894fade37
unknown <you@example.com>
2022-08-08 21:23:04 +0700

    T

--- Case Study/case_study_1.sql
+++ Case Study/case_study_1.sql
@@ -83,7 +83,7 @@ create table khach_hang(
 	ma_khach_hang int primary key,
     ho_ten varchar(45) not null,
     ngay_sinh date not null,
-    gioi_tinh bit (1),
+    gioi_tinh bit (1) not null,
     so_cmnd varchar(45) not null,
     so_dien_thoai varchar(45) not null,
     email varchar (45),
@@ -91,9 +91,9 @@ create table khach_hang(
 	ma_loai_khach int ,
     foreign key (ma_loai_khach) references loai_khach(ma_loai_khach)
 );
-insert into khach_hang
+insert into khach_hang(ma_khach_hang,ho_ten,ngay_sinh,gioi_tinh,so_cmnd,so_dien_thoai,email,dia_chi,ma_loai_khach)
 values 
-(1,'Nguyễn Thị Hào', '1970-11-07',0,'643431213','0945423362','thihao07@gmail.com','23 Nguyễn Hoàng, Đà Nẵng',5),
+(1,'Nguyễn Thị Hào','1970-11-07',0,'643431213','0945423362','thihao07@gmail.com','23 Nguyễn Hoàng, Đà Nẵng',5),
 (2,'Phạm Xuân Diệu','1992-08-08',1,'865342123','0954333333','xuandieu92@gmail.com','K77/22 Thái Phiên, Quảng Trị',3),
 (3,'Trương Đình Nghệ','1990-02-27',1,'488645199','0373213122','nghenhan2702@gmail.com','K323/12 Ông Ích Khiêm, Vinh',1),
 (4,'Dương Văn Quan','1981-07-08',1,'543432111','0490039241','duongquan@gmail.com','K453/12 Lê Lợi, Đà Nẵng',1),
@@ -102,7 +102,7 @@ values
 (7,'Nguyễn Mỹ Kim','1984-04-08',0,'856453123','0912345698','kimcuong84@gmail.com','K123/45 Lê Lợi, Hồ Chí Minh',1),
 (8,'Nguyễn Thị Hào','1999-04-08',0,'965656433','0763212345','haohao99@gmail.com','55 Nguyễn Văn Linh, Kon Tum',3),
 (9,'Trần Đại Danh','1994-07-01',1,'432341235','0643343433','danhhai99@gmail.com','24 Lý Thường Kiệt, Quảng Ngãi',1),
-(10,'Nguyễn Tâm Đắc','1989-07-01',1,'344343432','344343432','dactam@gmail.com','22 Ngô Quyền, Đà Nẵng',2);
+(10,'Nguyễn Tâm Đắc','1989-07-01',1,'344343432','0987654321','dactam@gmail.com','22 Ngô Quyền, Đà Nẵng',2);
 
 
 create table loai_dich_vu(
@@ -186,29 +186,29 @@ create table hop_dong(
 insert into hop_dong(ma_hop_dong, ngay_lam_hop_dong, ngay_ket_thuc,
  tien_dat_coc, ma_nhan_vien, ma_khach_hang, ma_dich_vu)
 values
-(1, '2020-12-08', '2020-12-08', 0, 3, 1,3);
--- (2,'2020-07-14','2020-07-21',200000,7,3,1),
--- (3,'2021-03-15','2021-03-17',50000,3,4,2),
--- (4,'2021-01-14','2021-01-18',100000,7,5,5),
--- (5,'2021-07-14','2021-07-15',0,7,2,6),
--- (6,'2021-06-01','2021-06-03',0,7,7,6),
--- (7,'2021-09-02','2021-09-05',100000,7,4,4),
--- (8,'2021-06-17','2021-06-18',150000,3,4,1),
--- (9,'2020-11-19','2020-11-19',0,3,4,3),
--- (10,'2021-04-12','2021-04-14',0,10,3,5),
--- (11,'2021-04-25','2021-04-25',0,2,2,1),
--- (12,'2021-05-25','2021-05-27',0,7,10,1);
+(1, '2020-12-08', '2020-12-08', 0, 3, 1,3),
+(2,'2020-07-14','2020-07-21',200000,7,3,1),
+(3,'2021-03-15','2021-03-17',50000,3,4,2),
+(4,'2021-01-14','2021-01-18',100000,7,5,5),
+(5,'2021-07-14','2021-07-15',0,7,2,6),
+(6,'2021-06-01','2021-06-03',0,7,7,6),
+(7,'2021-09-02','2021-09-05',100000,7,4,4),
+(8,'2021-06-17','2021-06-18',150000,3,4,1),
+(9,'2020-11-19','2020-11-19',0,3,4,3),
+(10,'2021-04-12','2021-04-14',0,10,3,5),
+(11,'2021-04-25','2021-04-25',0,2,2,1),
+(12,'2021-05-25','2021-05-27',0,7,10,1);
 
 create table hop_dong_chi_tiet(
 	ma_hop_dong_chi_tiet int primary key,
+    so_luong int not null,
     ma_hop_dong int,
     foreign key (ma_hop_dong) references hop_dong(ma_hop_dong),
     ma_dich_vu_di_kem int,
-    foreign key (ma_dich_vu_di_kem) references dich_vu_di_kem(ma_dich_vu_di_kem),
-    so_luong int not null
+    foreign key (ma_dich_vu_di_kem) references dich_vu_di_kem(ma_dich_vu_di_kem)
 );
 
-insert into hop_dong_chi_tiet(ma_hop_dong_chi_tiet, ma_hop_dong, ma_dich_vu, so_luong)
+insert into hop_dong_chi_tiet(ma_hop_dong_chi_tiet, so_luong, ma_hop_dong, ma_dich_vu_di_kem)
 values
 (1,5,2,4),
 (2,8,2,5),
