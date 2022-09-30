7116f731e08bd92c491511ad3fef51004f597d5e
unknown <you@example.com>
2022-09-30 09:19:19 +0700

    T

--- ss10_jsp_and_jstl/exercise/customer_list/src/main/java/controller/CustomerServlet.java
+++ ss10_jsp_and_jstl/exercise/customer_list/src/main/java/controller/CustomerServlet.java
@@ -16,8 +16,8 @@ public class CustomerServlet extends HttpServlet {
         request.setCharacterEncoding("UTF-8");
         List<Customer> customerList = new ArrayList<>();
 
-        customerList.add(new Customer("Đặng Thị Thủy Tiên", "20/03/2000", "Quảng Nam", "https://andy.codegym.vn/storage/images/avatars/38cba5c3296a2935a26f2dc896be48e2.jpeg"));
-        customerList.add(new Customer("Đặng Thị Thủy Tiên", "20/03/2000", "Quảng Nam", "https://andy.codegym.vn/storage/images/avatars/38cba5c3296a2935a26f2dc896be48e2.jpeg"));
+        customerList.add(new Customer("Đặng Thị Thủy Tiên", "20/03/2000", "Quảng Nam", "https://scontent.fsgn2-1.fna.fbcdn.net/v/t1.6435-9/155512900_562775151346347_5654354541835105790_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=EsmixynJoj4AX_nX6-h&_nc_ht=scontent.fsgn2-1.fna&oh=00_AT-UB36mMB_FfeB9kKciP5jivAyUdf-pbs-e73cjm4q-6g&oe=635D0A72"));
+        customerList.add(new Customer("Đặng Thị Thủy Tiên", "20/03/2000", "Quảng Nam", "https://scontent.fsgn2-1.fna.fbcdn.net/v/t1.6435-9/155512900_562775151346347_5654354541835105790_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=EsmixynJoj4AX_nX6-h&_nc_ht=scontent.fsgn2-1.fna&oh=00_AT-UB36mMB_FfeB9kKciP5jivAyUdf-pbs-e73cjm4q-6g&oe=635D0A72"));
         customerList.add(new Customer("Đặng Thị Thủy Tiên", "20/03/2000", "Quảng Nam", "https://andy.codegym.vn/storage/images/avatars/38cba5c3296a2935a26f2dc896be48e2.jpeg"));
         customerList.add(new Customer("Đặng Thị Thủy Tiên", "20/03/2000", "Quảng Nam", "https://andy.codegym.vn/storage/images/avatars/38cba5c3296a2935a26f2dc896be48e2.jpeg"));
 
--- ss11_mvc/practice/src/main/webapp/index.jsp
+++ ss11_mvc/practice/src/main/webapp/index.jsp
@@ -1,18 +1,16 @@
 <%--
   Created by IntelliJ IDEA.
   User: hp
-  Date: 9/29/2022
-  Time: 9:09 PM
+  Date: 9/30/2022
+  Time: 9:07 AM
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
-
 <html>
   <head>
-    <title>Index</title>
+    <title>$Title$</title>
   </head>
   <body>
-  <a href="/customer">Hiển thị Thông tin khách hàng!</a>
+  $END$
   </body>
 </html>
